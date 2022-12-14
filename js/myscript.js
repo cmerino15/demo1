59a1390ab3686e9b05db6a0b225afa9430aac6a7
cmerino15 <45248452+cmerino15@users.noreply.github.com>
2022-12-14 14:11:44 -0500

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,15 @@
+function ban(){
+  let x = null;
+  let elem = document.getElementById("banner");
+  let sx = 0;
+  clearInterval(x);
+  x = setInterval(frame,5);
+  function frame(){
+    if(sx == 500){
+     clearInterval(x);
+  }else{
+    sx++;
+    elem.style.left = sx + "px";
+  }
+  }
+}
